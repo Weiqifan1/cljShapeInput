3241e00ce4ef4f129910c6ccfbf84e5cdc07c1c7
Weiqifan1 <cmlykke@hotmail.com>
2020-10-25 10:51:21 +0100

    first code

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cljshapeinput
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cljshapeinput/core_test.clj
@@ -0,0 +1,7 @@
+(ns cljshapeinput.core-test
+  (:require [clojure.test :refer :all]
+            [cljshapeinput.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
