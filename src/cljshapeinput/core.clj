509ee581d303f9850311adc4369fa96463272cf1
Weiqifan1 <cmlykke@hotmail.com>
2020-10-29 16:08:35 +0100

    first clojure code

--- src/cljshapeinput/core.clj
+++ src/cljshapeinput/core.clj
@@ -1,6 +1,17 @@
 (ns cljshapeinput.core)
 
+(load-file "src/cljshapeinput/helper.clj")
+
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn -main [& args]
+  "I don't do a whole lot."
+  (println (str "Hello, World! 2" 5 (helper_addNumbers 3 4))))
+
+
+
+
+;slut
---
+++ src/cljshapeinput/helper.clj
@@ -0,0 +1,12 @@
+
+
+(defn helper_addNumbers
+  "add two numbers"
+  [numOne numTwo]
+  (+ numOne numTwo))
+
+
+
+
+
+;slut
